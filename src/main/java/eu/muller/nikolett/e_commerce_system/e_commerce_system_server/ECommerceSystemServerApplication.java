0929e3a4d20f7f15f1c0dbd467b8edf6a200998f
nikolettmuller <nikolett.muller98@gmail.com>
2024-10-09 19:32:04 +0200

    Create base project.

---
+++ src/main/java/eu/muller/nikolett/e_commerce_system/e_commerce_system_server/ECommerceSystemServerApplication.java
@@ -0,0 +1,13 @@
+package eu.muller.nikolett.e_commerce_system.e_commerce_system_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSystemServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/muller/nikolett/e_commerce_system/e_commerce_system_server/ECommerceSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.muller.nikolett.e_commerce_system.e_commerce_system_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSystemServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
