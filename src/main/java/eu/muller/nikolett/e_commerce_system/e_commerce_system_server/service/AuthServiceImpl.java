381e84c32649600c3991d4cc5baf0382bff31ccd
nikolettmuller <nikolett.muller98@gmail.com>
2024-10-10 18:59:13 +0200

    Add register impl.

--- src/main/java/eu/muller/nikolett/e_commerce_system/e_commerce_system_server/service/AuthServiceImpl.java
+++ src/main/java/eu/muller/nikolett/e_commerce_system/e_commerce_system_server/service/AuthServiceImpl.java
@@ -2,6 +2,8 @@ package eu.muller.nikolett.e_commerce_system.e_commerce_system_server.service;
 
 import eu.muller.nikolett.e_commerce_system.e_commerce_system_server.dto.RegisterRequest;
 import eu.muller.nikolett.e_commerce_system.e_commerce_system_server.dto.RegisterResponse;
+import eu.muller.nikolett.e_commerce_system.e_commerce_system_server.entity.User;
+import eu.muller.nikolett.e_commerce_system.e_commerce_system_server.mapper.RegisterMapper;
 import eu.muller.nikolett.e_commerce_system.e_commerce_system_server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,8 +14,16 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
 
+    private final RegisterMapper registerMapper;
+
     @Override
     public RegisterResponse register(RegisterRequest register) {
-        return null;
+
+        User user = registerMapper.map(register);
+        userRepository.save(user);
+
+        RegisterResponse response = new RegisterResponse();
+        response.setName(user.getName());
+        return response;
     }
 }
