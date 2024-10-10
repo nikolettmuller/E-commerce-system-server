98e5495190efe109da42a5e4965db2cd68024ee5
nikolettmuller <nikolett.muller98@gmail.com>
2024-10-10 19:41:01 +0200

    Create auth controller with register api.

---
+++ src/main/java/eu/muller/nikolett/e_commerce_system/e_commerce_system_server/controller/AuthController.java
@@ -0,0 +1,24 @@
+package eu.muller.nikolett.e_commerce_system.e_commerce_system_server.controller;
+
+
+import eu.muller.nikolett.e_commerce_system.e_commerce_system_server.dto.RegisterRequest;
+import eu.muller.nikolett.e_commerce_system.e_commerce_system_server.dto.RegisterResponse;
+import eu.muller.nikolett.e_commerce_system.e_commerce_system_server.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/users")
+    public RegisterResponse register(@RequestBody RegisterRequest register) {
+        return authService.register(register);
+    }
+}
